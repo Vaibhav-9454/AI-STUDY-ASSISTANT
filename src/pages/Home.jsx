61ee4355bda3b2645da9a8e95d41d8b6d7c95895
Vaibhav Tiwari <vaibhavt7911@gmail.com>
2026-07-17 00:29:53 +0530

    Connect frontend with backend service

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
+
 import Navbar from "../components/layout/Navbar";
 import Hero from "../components/home/Hero";
-import InputBox from "../components/study/InputBox";
+import InputBox from "../components/home/InputBox";
 
 function Home() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <InputBox />
-    </>
-  );
+
+    const [studyData, setStudyData] = useState(null);
+
+    return (
+        <>
+            <Navbar />
+
+            <Hero />
+
+            <InputBox
+                setStudyData={setStudyData}
+            />
+
+            {studyData && (
+
+                <div>
+
+                    <h2>Study Kit Generated</h2>
+
+                    <pre>
+                        {JSON.stringify(studyData, null, 2)}
+                    </pre>
+
+                </div>
+
+            )}
+
+        </>
+    );
+
 }
 
 export default Home;
